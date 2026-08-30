6ced609d9dcc461ce24cafc49c9218b8f65c4a3b
Ananth Bhat <ananthbhat1527@gmail.com>
2026-08-30 10:26:44 +0530

    Fix email confirmation callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,29 @@
+import { type EmailOtpType } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/dashboard'
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
+    if (!error) {
+      const forwardedHost = request.headers.get('x-forwarded-host')
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
+    }
+  }
 
   if (code) {
     const supabase = await createClient()
